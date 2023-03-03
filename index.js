ecfef4fd5d53bb365692897f0273591046c66df8
spirit1269 <kimberlydjenkins1@gmail.com>
2023-03-03 17:08:10 -0600

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-(function() {
-  'use strict';
+const stopButton = document.getElementById("stopButton");
+const slowButton = document.getElementById("slowButton");
+const goButton = document.getElementById("goButton");
+const stopLight = document.getElementById("stopLight");
+const slowLight = document.getElementById("slowLight");
+const goLight = document.getElementById("goLight");
 
-  // YOUR CODE HERE
-})();
+stopButton.addEventListener("click", function(){
+  if (stopLight.style.backgroundColor === "red"){
+    stopLight.style.backgroundColor = "black"
+  } else {
+    stopLight.style.backgroundColor = "red";
+  }
+});
+slowButton.addEventListener("click", function(){
+  if ( slowLight.style.backgroundColor === "yellow"){
+      slowLight.style.backgroundColor = "black";
+  } else {
+    slowLight.style.backgroundColor = "yellow";
+  }
+});
+goButton.addEventListener("click", function(){
+  if (goLight.style.backgroundColor === 'green') {
+    goLight.style.backgroundColor = "black";
+  } else {
+    goLight.style.backgroundColor = "green";
+  }
+});
+stopButton.addEventListener("click", function(){
+  if (stopLight.style.backgroundColor === "red"){
+    stopLight.style.backgroundColor = "black"
+  } else {
+    stopLight.style.backgroundColor = "red";
+  }
+});
+slowButton.addEventListener("click", function(){
+  if ( slowLight.style.backgroundColor === "yellow"){
+      slowLight.style.backgroundColor = "black";
+  } else {
+    slowLight.style.backgroundColor = "yellow";
+  }
+});
+goButton.addEventListener("click", function(){
+  if (goLight.style.backgroundColor === 'green') {
+    goLight.style.backgroundColor = "black";
+  } else {
+    goLight.style.backgroundColor = "green";
+  }
+});
+
